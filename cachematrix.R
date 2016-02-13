b5ad4e55248ff29c30f53b0471e1bce71ab54bed
VadikB <bogachevv@mail.ru>
2016-02-13 11:49:45 +0300

    Revert "cachematrix"
    This reverts commit da7bb2be0f06ebc9040c8f3c2b05033f404a0a50.

--- cachematrix.R
+++ cachematrix.R
@@ -1,38 +1,15 @@
-## The function is prepating environment for using cashSolve just like it was made in the example
-## usage :
-# x1 <- makeCacheMatrix(x) - to prepare the cache
-# cacheSolve(x1)           - to get the inverse matrix, trying to get it from cache
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y)
-  {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function()
-    x
-  setSolve <- function(slv)
-    m <<- slv
-  getSolve <- function()
-    m
-  list(
-    set = set, get = get,
-    setSolve = setSolve,
-    getSolve = getSolve
-  )
+
 }
 
-## The function tryies to get inverse matrix from cache first, uses solve function if inverse matrix is still not in cache
+
+## Write a short comment describing this function
+
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  m <- x$getSolve()
-  if (!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setSolve(m)
-  m
+        ## Return a matrix that is the inverse of 'x'
 }
